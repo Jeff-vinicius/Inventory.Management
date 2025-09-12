58c249ae426d490f7f62521809711ec75b23ede7
Jeff-vinicius <jeffersonshady10@gmail.com>
2025-09-12 20:44:14 -0300

    feat: creation of the shared kernel layer

---
+++ src/Inventory.Management.SharedKernel/Inventory.Management.SharedKernel/ErrorType.cs
@@ -0,0 +1,11 @@
+﻿namespace Inventory.Management.SharedKernel
+{
+    public enum ErrorType
+    {
+        Failure = 0,
+        Validation = 1,
+        Problem = 2,
+        NotFound = 3,
+        Conflict = 4
+    }
+}
